8aec30834a39012439da5a31a13d56f2e907fa69
Johnathan Ingle <jingle1000@gmail.com>
2019-02-23 08:02:02 -0500

    Create Account Page Connection List
    The Account page shows your open connections

--- src/user/views.py
+++ src/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from api.models import ProfileConnection
 
 # Create your views here.
 def register(request):
@@ -19,5 +20,17 @@ def login(request):
     return render(request, 'user/login.html')
 
 def account(request):
-    return render(request, 'user/account.html')
+    user_connections = ProfileConnection.objects.filter(profile__user__username=request.user.username)
+    user_data = []
+    for connection in user_connections:
+        c_type = connection.connection.type_id.connection_type
+        c_name = connection.connection.name
+        if c_type.startswith("Optional"):
+            c_type = c_type[8:]
+        dataset = f'Your {c_type} include {c_name}'
+        user_data.append(dataset)
+    context = {
+        "connections":user_data
+    }
+    return render(request, 'user/account.html', context)
 
