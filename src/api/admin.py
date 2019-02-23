54a2b82e9a2c0e8c8bc7bc35d2484607c769b181
Johnathan Ingle <jingle1000@gmail.com>
2019-02-22 23:36:50 -0500

    add-profile-connections

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,3 +3,4 @@ from . import models
 # Register your models here.
 admin.site.register(models.Connection)
 admin.site.register(models.Type)
+admin.site.register(models.ProfileConnection)
--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from user import models as user_models
 # Create your models here.
 class Type(models.Model):
     connection_type = models.CharField(max_length=80)
@@ -11,3 +11,7 @@ class Connection(models.Model):
     type_id = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return f"{self.name}"
+
+class ProfileConnection(models.Model):
+    profile = models.ForeignKey(user_models.Profile, on_delete=models.CASCADE)
+    connection = models.ForeignKey(Connection, on_delete=models.CASCADE)
