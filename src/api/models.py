9b2e826bb710b0226f9a8c3277e0e7b80a61660d
Johnathan Ingle <jingle1000@gmail.com>
2019-02-22 23:14:25 -0500

    Add user profile
    User profile is now created when the user registers for the site

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Type(models.Model):
+    connection_type = models.CharField(max_length=80)
+    def __str__(self):
+        return self.connection_type
+
+class Connection(models.Model):
+    name = models.CharField(max_length=80)
+    type_id = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return f"{self.name}"
